d58491befadd4fa05291d86af4d7c0031f359aa2
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2025-05-27 11:34:07 -0500

    d
    d

--- api/assistants/openai_client.py
+++ api/assistants/openai_client.py
@@ -2,7 +2,9 @@ from dotenv import load_dotenv
 import os
 from openai import OpenAI
 
-load_dotenv()
+# Ruta relativa al directorio del script (openai_client.py)
+env_path = Path(__file__).resolve().parent.parent.parent / ".env"  # Ajusta según tu estructura
+load_dotenv(dotenv_path=env_path)
 
 # Configuración inicial
 API_KEY = os.getenv("API_KEY_IA")
